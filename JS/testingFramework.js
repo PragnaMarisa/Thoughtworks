7a25f36f8dac9a53c7adc161ecfcfa4092b07a36
Pragna Marisa <158675244+PragnaMarisa@users.noreply.github.com>
2024-11-30 13:19:26 +0530

    Update testingFramework.js

--- JS/testingFramework.js
+++ JS/testingFramework.js
@@ -1,5 +1,30 @@
+// testing part...
+
+function getMaxLengthOfData(testedData, colIndex) {
+  let maxLength = 0;
+
+  for (let index = 0; index < testedData.length; index++) {
+    const currData = testedData[index][colIndex];
+    if (currData.length > maxLength) {
+      maxLength = currData.length;
+    }
+  }
+
+  return maxLength;
+}
+
+function getMaximumLengthsOf(testedData, columns) {
+  const lengths = [];
+
+  for (let index = 0; index < columns.length; index++) {
+    lengths.push(getMaxLengthOfData(testedData, columns[0]) + 4);
+  }
+
+  return lengths;
+}
+
 function repeat(string, noOfTimes) {
-  if (noOfTimes === 0) {
+  if (noOfTimes <= 0) {
     return '';
   }
 
@@ -14,39 +39,68 @@ function createHyphenLine(length) {
   return repeat('-', length);
 }
 
-function createRow(columns, columnLength, isHeader) {
+function createRow(columns, columnLengths) {
   let finalRow = '';
 
-  let rowCreated = '|';
-  for (let row = 0; row < columns.length; row++) {
-    rowCreated += '  ' + columns[row];
-    const usedSpace = rowCreated.length + (row === 0 && !isHeader ? 2 : 1);
-    rowCreated += createSpacedString(columnLength - usedSpace) + '|';
-    finalRow = finalRow + rowCreated;
-    rowCreated = '';
+  let row = '|';
+  for (let column = 0; column < columns.length; column++) {
+    row += '  ' + columns[column];
+    row += createSpacedString(columnLengths[column] - row.length) + '|';
+    finalRow = finalRow + row;
+    row = '';
   }
 
-  console.log(finalRow);
-  return finalRow.length;
+  return finalRow;
 }
 
-function createHeader(headers) {
-  const headerLength = 15 * headers.length;
+function createHeader(headers, inputDataLengths) {
+  const headerLength = createRow(headers, inputDataLengths).length;
   console.log(createHyphenLine(headerLength));
-  createRow(headers, 15, true);
+  console.log(createRow(headers, inputDataLengths));
   console.log(createHyphenLine(headerLength));
 }
 
-function createResultsRow(rows) {
-  let hyphenLength = 0;
+function createResultsRow(rows, inputDataLengths) {
   for (let index = 0; index < rows.length; index++) {
-    hyphenLength = createRow(rows[index], 15, false);
+    const hyphenLength = createRow(rows[index], inputDataLengths).length;
+
+    console.log(createRow(rows[index], inputDataLengths));
+    console.log(createHyphenLine(hyphenLength));
   }
-  console.log(createHyphenLine(hyphenLength));
 }
 
-const headers = ["Status", "Number", "Array1", "Actual", "Expected"];
-const testedData = [['✅', 3, 4, 600, 712], ['✅', 4, 5, 246, 123], ['✅', 4, 5, 323, 5665]];
+function createTestsTable(headers, testedData) {
+  const inputDataLengths = getMaximumLengthsOf(testedData, [1, 2, 3]);
+  inputDataLengths.unshift(10);
 
-createHeader(headers);
-createResultsRow(testedData);
+  createHeader(headers, inputDataLengths);
+  inputDataLengths[0] = inputDataLengths[0] - 1;
+  createResultsRow(testedData, inputDataLengths);
+}
+
+function getMark(isPassed) {
+  return isPassed ? '✅' : '❌';
+}
+
+function testStrictlyAscending(array, expected) {
+  const actual = isStrictlyAscending(array);
+  const isPassed = actual === expected;
+
+  return [getMark(isPassed), '[' + array + ']', actual, expected];
+}
+
+function testAll() {
+  const headers = ["Status", "Array", "Actual", "Expected"];
+  const testedData = [];
+  testedData.push(testStrictlyAscending([1, 3, 5, 6, 8, 7], false));
+  testedData.push(testStrictlyAscending([1, 3, 8, 5, 6, 8, 7], false));
+  testedData.push(testStrictlyAscending([1, 3, 5, 6, 7], true));
+  testedData.push(testStrictlyAscending([], true));
+  testedData.push(testStrictlyAscending([1], true));
+  testedData.push(testStrictlyAscending([1, 3, 3, 4], false));
+  testedData.push(testStrictlyAscending([2, 1], false));
+  testedData.push(testStrictlyAscending([1, 31, 3, 4], false));
+  createTestsTable(headers, testedData);
+}
+
+testAll();
